3ecea576bd185984e6e69de2deca256d07ddaf62
Liz <elizabethqwang@gmail.com>
2019-12-04 17:31:21 -0800

    outlet to favorite button

--- build/VisionSample.build/Debug-iphonesimulator/VisionSample.build/DerivedSources/CoreDataGenerated/Images/Images+CoreDataModel.swift
+++ build/VisionSample.build/Debug-iphonesimulator/VisionSample.build/DerivedSources/CoreDataGenerated/Images/Images+CoreDataModel.swift
@@ -2,7 +2,7 @@
 //  Images+CoreDataModel.swift
 //  
 //
-//  Created by Liz W on 11/26/19.
+//  Created by Liz W on 12/4/19.
 //
 //  This file was automatically generated and should not be edited.
 //
